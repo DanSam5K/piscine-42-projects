fa03ea5f9a0d2b3d678d6cb6beb127cd368cedbd
Daniel Samuel <dansam5000@gmail.com>
2024-03-12 13:24:08 +0100

    Ex05 print combination

--- c-00/ex05/ft_print_comb.c
+++ c-00/ex05/ft_print_comb.c
@@ -6,7 +6,7 @@
 /*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 12:51:38 by dansam            #+#    #+#             */
-/*   Updated: 2024/03/12 13:21:48 by dansam           ###   ########.fr       */
+/*   Updated: 2024/03/12 13:22:35 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,33 @@ void ft_put_comb(char a, char b, char c)
   ft_putchar(',');
   ft_putchar(' ');
 }
+
+void ft_print_comb(void)
+{
+  int a;
+  int b;
+  int c;
+
+  a = 0;
+  while (a <= 7)
+  {
+    b = a + 1;
+    while (b <= 8)
+    {
+      c = b + 1;
+      while (c <= 9)
+      {
+        ft_put_comb(a, b, c);
+        c++;
+      }
+      b++;
+    }
+    a++;
+  }
+}
+
+// int main()
+// {
+//   ft_print_comb();
+//   return 0;
+// }
