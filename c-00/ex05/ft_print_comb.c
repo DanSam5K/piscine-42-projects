8f0d03b7eee74eecfa85d75a1c0438872b35331b
Daniel Samuel <dansam5000@gmail.com>
2024-03-12 13:22:20 +0100

    Print combination

--- c-00/ex05/ft_print_comb.c
+++ c-00/ex05/ft_print_comb.c
@@ -6,11 +6,24 @@
 /*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 12:51:38 by dansam            #+#    #+#             */
-/*   Updated: 2024/03/12 12:53:53 by dansam           ###   ########.fr       */
+/*   Updated: 2024/03/12 13:21:48 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
 
 void ft_putchar(char c)
 {
   write(1, &c, 1);
+}
+
+void ft_put_comb(char a, char b, char c)
+{
+  ft_putchar(a + '0');
+  ft_putchar(b + '0');
+  ft_putchar(c + '0');
+  if (a == 7 && b == 8 && c == 9)
+    return;
+  ft_putchar(',');
+  ft_putchar(' ');
 }
