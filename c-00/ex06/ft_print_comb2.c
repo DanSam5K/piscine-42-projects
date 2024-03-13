ba2507ef91955c61423f892ece1b4e214553b0cd
Daniel Samuel <dansam5000@gmail.com>
2024-03-13 20:19:09 +0100

    fft int to char func

--- c-00/ex06/ft_print_comb2.c
+++ c-00/ex06/ft_print_comb2.c
@@ -6,7 +6,7 @@
 /*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 18:07:16 by dansam            #+#    #+#             */
-/*   Updated: 2024/03/13 18:07:30 by dansam           ###   ########.fr       */
+/*   Updated: 2024/03/13 20:17:58 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,15 @@ void ft_putchar(char c)
 {
   write(1, &c, 1);
 }
+
+void ft_int_to_char(int n)
+{
+  ft_putchar(n / 10 + '0');
+  ft_putchar(n % 10 + '0');
+}
+
+// int main(void)
+// {
+//   ft_print_comb2();
+//   return 0;
+// }
