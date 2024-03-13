9f201c8c79fce4cfd99fcc91746a85d005ac8858
Daniel Samuel <dansam5000@gmail.com>
2024-03-13 20:20:19 +0100

    print combination 2

--- c-00/ex06/ft_print_comb2.c
+++ c-00/ex06/ft_print_comb2.c
@@ -6,7 +6,7 @@
 /*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 18:07:16 by dansam            #+#    #+#             */
-/*   Updated: 2024/03/13 20:17:58 by dansam           ###   ########.fr       */
+/*   Updated: 2024/03/13 20:19:20 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,31 @@ void ft_int_to_char(int n)
 {
   ft_putchar(n / 10 + '0');
   ft_putchar(n % 10 + '0');
+}
+
+void ft_print_comb2(void)
+{
+  int i;
+  int j;
+
+  i = 0;
+  while (i <= 98)
+  {
+    j = i + 1;
+    while (j <= 99)
+    {
+      ft_int_to_char(i);
+      ft_putchar(' ');
+      ft_int_to_char(j);
+      if (i != 98)
+      {
+        ft_putchar(',');
+        ft_putchar(' ');
+      }
+      j++;
+    }
+    i++;
+  }
 }
 
 // int main(void)
