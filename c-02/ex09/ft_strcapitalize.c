77a255f03722451990be80e60796940da00bec75
Daniel Samuel <dansam5000@gmail.com>
2024-03-22 12:37:46 +0100

    check if char is up/lowercase

--- c-02/ex09/ft_strcapitalize.c
+++ c-02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 12:20:20 by dansam            #+#    #+#             */
-/*   Updated: 2024/03/22 12:35:31 by dansam           ###   ########.fr       */
+/*   Updated: 2024/03/22 12:36:42 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,14 @@
 int ft_check_alphanumeric(char c)
 {
 	return ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+}
+
+int ft_check_lowercase(char c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
+int ft_check_uppercase(char c)
+{
+	return (c >= 'A' && c <= 'Z');
 }
