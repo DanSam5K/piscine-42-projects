1d6c5044285c67905cf4bd33c73d73a17ea9d125
Daniel Samuel <dansam5000@gmail.com>
2024-03-22 12:41:01 +0100

    Ex09 Capitalise each word first char

--- c-02/ex09/ft_strcapitalize.c
+++ c-02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 12:20:20 by dansam            #+#    #+#             */
-/*   Updated: 2024/03/22 12:36:42 by dansam           ###   ########.fr       */
+/*   Updated: 2024/03/22 12:39:11 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,3 +26,29 @@ int ft_check_uppercase(char c)
 {
 	return (c >= 'A' && c <= 'Z');
 }
+
+char *ft_strcapitalize(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i == 0 || !ft_check_alphanumeric(str[i - 1]))
+		{
+			if (ft_check_lowercase(str[i]))
+				str[i] -= 32;
+		}
+		else if (ft_check_uppercase(str[i]))
+			str[i] += 32;
+		i++;
+	}
+	return (str);
+}
+
+// int main(void)
+// {
+// 	char str[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+// 	printf("%s\n", ft_strcapitalize(str));
+// 	return (0);
+// }
