c46d188a25134914124b7fd77dbf3dcf95e566bf
Daniel Samuel <dansam5000@gmail.com>
2024-03-24 22:38:25 +0100

    Ex00 strncmp reproduce the func strncmp

---
+++ c-03/ex01/ft_strncmp.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/24 22:31:45 by dansam            #+#    #+#             */
+/*   Updated: 2024/03/24 22:36:39 by dansam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "stdio.h"
+
+int ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s1[i] && s2[i] && i < n)
+	{
+		if (s1[i] - s2[i] != 0)
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	if (i == n)
+		return (0);
+	return (s1[i] - s2[i]);
+}
+
+int main()
+{
+	char s1[] = "Helloa";
+	char s2[] = "Hello";
+	printf("%d\n", ft_strncmp(s1, s2, 5));
+	return (0);
+}
