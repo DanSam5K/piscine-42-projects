c901170224e8bb1360e06673bd285bcb48f8073e
Daniel Samuel <dansam5000@gmail.com>
2024-03-27 15:18:38 +0100

    Print all posbile number int

---
+++ c-04/ex02/ft_putnbr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/27 15:11:07 by dansam            #+#    #+#             */
+/*   Updated: 2024/03/27 15:17:13 by dansam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int nb)
+{
+	long nbr;
+
+	nbr = nb;
+	if (nbr < 0)
+	{
+		ft_putchar('-');
+		nbr = -nbr;
+	}
+	if (nbr >= 10)
+		ft_putnbr(nbr / 10);
+	ft_putchar(nbr % 10 + '0');
+}
+
+// int main()
+// {
+// 	ft_putnbr(-2147483648);
+// 	return (0);
+// }
