638fa6d4e498985e17be888a47411f6cf46a0c18
Daniel Samuel <dansam5000@gmail.com>
2024-03-27 15:39:44 +0100

    ft check base

--- c-04/ex04/ft_putnbr_base.c
+++ c-04/ex04/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 15:27:29 by dansam            #+#    #+#             */
-/*   Updated: 2024/03/27 15:36:45 by dansam           ###   ########.fr       */
+/*   Updated: 2024/03/27 15:39:20 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,28 @@ int ft_strlen(char *str)
 	while (str[i])
 		i++;
 	return (i);
+}
+
+int ft_check_base(char *base)
+{
+	int i;
+	int j;
+
+	i = 0;
+	if (ft_strlen(base) < 2)
+		return (0);
+	while (base[i])
+	{
+		if (base[i] == '+' || base[i] == '-' || base[i] == ' ' || (base[i] >= 9 && base[i] <= 13))
+			return (0);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
 }
