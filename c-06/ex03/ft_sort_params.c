79f5f8dcf84717a4d99e73d065f6de725ca989ec
Daniel Samuel <dansam5000@gmail.com>
2024-04-02 13:31:55 +0200

    sort strcmp char

--- c-06/ex03/ft_sort_params.c
+++ c-06/ex03/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 13:28:35 by dansam            #+#    #+#             */
-/*   Updated: 2024/04/02 13:29:31 by dansam           ###   ########.fr       */
+/*   Updated: 2024/04/02 13:30:26 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,23 +25,4 @@ int ft_strcmp(char *s1, char *s2)
 	while (s1[i] && s2[i] && (s1[i] == s2[i]))
 		i++;
 	return (s1[i] - s2[i]);
-}
-
-void ft_print_params(int argc, char **argv)
-{
-	int i;
-	int j;
-
-	i = 1;
-	while (i < argc)
-	{
-		j = 0;
-		while (argv[i][j])
-		{
-			ft_putchar(argv[i][j]);
-			j++;
-		}
-		ft_putchar('\n');
-		i++;
-	}
 }
