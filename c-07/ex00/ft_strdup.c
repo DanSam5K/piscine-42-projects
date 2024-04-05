b6e09139dddae249fdd6ba631c669edf82fb616d
Daniel Samuel <dansam5000@gmail.com>
2024-04-05 15:01:43 +0200

    Ex00 reproudce strdup behaviour

--- c-07/ex00/ft_strdup.c
+++ c-07/ex00/ft_strdup.c
@@ -6,11 +6,12 @@
 /*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 21:06:04 by dansam            #+#    #+#             */
-/*   Updated: 2024/04/05 14:03:14 by dansam           ###   ########.fr       */
+/*   Updated: 2024/04/05 15:00:45 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 
 unsigned int ft_strlen(char *str)
 {
@@ -35,3 +36,23 @@ char *ft_str_cpy(char *dest, char *src)
 	dest[i] = '\0';
 	return (dest);
 }
+
+char *ft_strdup(char *src)
+{
+	char *dest;
+
+	dest = (char *)malloc(sizeof(char) * (ft_strlen(src) + 1));
+	if (dest == NULL)
+		return (NULL);
+	ft_str_cpy(dest, src);
+	return (dest);
+}
+
+// int main()
+// {
+// 	char *str = "Hello World!";
+// 	char *copy = ft_strdup(str);
+// 	printf("%s\n", copy);
+// 	free(copy);
+// 	return (0);
+// }
