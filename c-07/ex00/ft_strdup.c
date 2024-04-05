2c4020995d83a61c6e0d9938cd895b4631adfb02
Daniel Samuel <dansam5000@gmail.com>
2024-04-05 14:06:11 +0200

    ft str copy

--- c-07/ex00/ft_strdup.c
+++ c-07/ex00/ft_strdup.c
@@ -6,15 +6,15 @@
 /*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 21:06:04 by dansam            #+#    #+#             */
-/*   Updated: 2024/04/04 21:43:51 by dansam           ###   ########.fr       */
+/*   Updated: 2024/04/05 14:03:14 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int ft_strlen(char *str)
+unsigned int ft_strlen(char *str)
 {
-	int i;
+	unsigned int i;
 
 	i = 0;
 	while (str[i])
@@ -22,15 +22,11 @@ int ft_strlen(char *str)
 	return (i);
 }
 
-char *ft_strdup(char *src)
+char *ft_str_cpy(char *dest, char *src)
 {
-	int i;
-	char *dest;
+	unsigned int i;
 
 	i = 0;
-	dest = (char *)malloc(sizeof(char) * (ft_strlen(src) + 1));
-	if (dest == NULL)
-		return (NULL);
 	while (src[i])
 	{
 		dest[i] = src[i];
@@ -38,13 +34,4 @@ char *ft_strdup(char *src)
 	}
 	dest[i] = '\0';
 	return (dest);
-}
-
-int main()
-{
-	char *src = "Hello World!";
-	char *dest = ft_strdup(src);
-	printf("%s\n", dest);
-	free(dest);
-	return (0);
 }
