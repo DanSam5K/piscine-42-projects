df75876e722ab827c36a23123d5013d3d72a6114
Daniel Samuel <dansam5000@gmail.com>
2024-04-06 13:38:33 +0200

    Ex01 ft range returns array

---
+++ c-07/ex01/ft_range.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/06 13:30:10 by dansam            #+#    #+#             */
+/*   Updated: 2024/04/06 13:33:02 by dansam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int *ft_range(int min, int max)
+{
+	int *range;
+	int i;
+
+	if (min >= max)
+		return (NULL);
+	range = (int *)malloc(sizeof(int) * (max - min));
+	if (range == NULL)
+		return (NULL);
+	i = 0;
+	while (min < max)
+	{
+		range[i] = min;
+		min++;
+		i++;
+	}
+	return (range);
+}
