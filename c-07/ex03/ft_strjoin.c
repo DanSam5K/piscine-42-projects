ef81535a9be8d18abb77f01f2827ac148e70e960
Daniel Samuel <dansam5000@gmail.com>
2024-04-08 13:05:23 +0200

    Create strn with sep

--- c-07/ex03/ft_strjoin.c
+++ c-07/ex03/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 13:00:48 by dansam            #+#    #+#             */
-/*   Updated: 2024/04/08 13:01:10 by dansam           ###   ########.fr       */
+/*   Updated: 2024/04/08 13:05:15 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,38 @@ unsigned int ft_strlen(char *str)
 	while (str[i])
 		i++;
 	return (i);
+}
+
+char *ft_create_str(int size, char **strs, char *sep)
+{
+	char *str;
+	int i;
+	int j;
+	int k;
+
+	str = (char *)malloc(sizeof(char) * size + 1);
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	k = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (strs[i][j])
+		{
+			str[k] = strs[i][j];
+			j++;
+			k++;
+		}
+		j = 0;
+		while (sep[j] && i < size - 1)
+		{
+			str[k] = sep[j];
+			j++;
+			k++;
+		}
+		i++;
+	}
+	str[k] = '\0';
+	return (str);
 }
