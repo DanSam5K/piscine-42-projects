b09b19628abda172b39df7da7ea1d09ad310690c
Daniel Samuel <dansam5000@gmail.com>
2024-05-04 03:55:18 +0200

    create ft header file

---
+++ c-projects/c-08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/04 03:51:38 by dansam            #+#    #+#             */
+/*   Updated: 2024/05/04 03:54:25 by dansam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+#define FT_H
+
+void ft_putchar(char c);
+void ft_putstr(char *str);
+void ft_swap(int *a, int *b);
+int ft_strcmp(char *s1, char *s2);
+int ft_strlen(char *str);
+
+#endif
