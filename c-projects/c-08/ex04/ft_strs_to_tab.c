738603cee12746c8d33634cd99cdb3de06fde737
Daniel Samuel <dansam5000@gmail.com>
2024-05-31 20:25:27 +0200

    add stock strh file

--- c-projects/c-08/ex04/ft_strs_to_tab.c
+++ c-projects/c-08/ex04/ft_strs_to_tab.c
@@ -6,7 +6,7 @@
 /*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 20:10:53 by dansam            #+#    #+#             */
-/*   Updated: 2024/05/31 20:15:25 by dansam           ###   ########.fr       */
+/*   Updated: 2024/05/31 20:21:37 by dansam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,4 +41,26 @@ char *ft_strcpy(char *str)
 	}
 	dest[i] = '\0';
 	return (dest);
+}
+
+struct s_stock_str *ft_strs_to_tab(int ac, char **av)
+{
+	t_stock_str *tab;
+	int i;
+
+	tab = (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1));
+	if (!tab)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		tab[i].size = ft_strlen(av[i]);
+		tab[i].str = av[i];
+		tab[i].copy = ft_strcpy(av[i]);
+		i++;
+	}
+	tab[i].str = 0;
+	tab[i].size = 0;
+	tab[i].copy = 0;
+	return (tab);
 }
