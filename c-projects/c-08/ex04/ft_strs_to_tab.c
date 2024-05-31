168724f79f56740527b71f10cf8b081f7e0fbe11
Daniel Samuel <dansam5000@gmail.com>
2024-05-31 20:15:42 +0200

    strlen func

---
+++ c-projects/c-08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dansam <dansam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/31 20:10:53 by dansam            #+#    #+#             */
+/*   Updated: 2024/05/31 20:15:25 by dansam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ft_stock_str.h"
+
+int ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char *ft_strcpy(char *str)
+{
+	char *dest;
+	int i;
+
+	dest = (char *)malloc(sizeof(char) * (ft_strlen(src) + 1));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
